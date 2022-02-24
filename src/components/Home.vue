8d705bc8e3a88fa162ffbae0767cd0f7fec860e8
jferreiro <fb.jdavid@gmail.com>
2022-02-24 09:24:54 -0400

    Modificar el menu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,12 +7,13 @@
             <img src="../assets/logoHG.png" style="width: 60px" />
           </div>
           <div class="left subtitulo">
-            <span class="textoRojo bordeTextoRojo1"
-              >Hermandad Gallega de Venezuela</span
-            ><br />
-            <span class="textoAmarillo bordeTextoNegro1"
-              >La segunda casa de los gallegos en Caracas</span
-            >
+            <span class="textoRojo bordeTextoAmarillo1 bold">
+              Hermandad Gallega de Venezuela
+            </span>
+            <br />
+            <span class="textoAmarillo bordeTextoRojo1">
+              Galicia en Caracas
+            </span>
           </div>
         </div>
         <div class="right">
@@ -21,8 +22,8 @@
               <p>Fecha: {{ currentDate() }}</p>
             </div>
             <div>
-              <font-awesome-icon icon="globe" size="1x" />&nbsp;Idioma: &nbsp;
-              <select id="selLang">
+              <font-awesome-icon icon="globe" size="1x" /> &nbsp;Idioma: &nbsp;
+              <select class="form-control" id="selLang">
                 <option value="spa" selected>Español</option>
                 <option value="gal">Gallego</option>
                 <option value="eng">Inglés</option>
@@ -31,100 +32,323 @@
           </div>
         </div>
       </div>
-      <div class="dropdown">
-        <button
-          class="btn btn-default dropdown-toggle"
-          type="button"
-          data-toggle="dropdown"
-        >
-          Dropdown <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu">
-          <li v-for="item in menuItems" :key="item" :class="{ 'dropdown-submenu': item.children }">
-            <a class="test" tabindex="-1" href="#">
-                {{ item.name }}<span class="caret" v-if="item.children"></span>
-            </a>
-            <ul class="dropdown-menu" v-if="item.children">
-              <li v-for="child in item.children" :key="child">
-                <a tabindex="-1" href="#" @click="setSelectedItem(child.name)">
-                    {{ child.name }}
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+      <ul id="menuDD" class="dropdown">
+        <li v-for="menu in primaryMenu" :key="menu">
+          <a href="#"> {{ menu.name }} <span v-if="menu.children"><i class="fas fa-caret-down"></i> </span> </a>
+          <ul v-if="menu.children" style="margin-left: -28px;">
+            <li v-for="child in menu.children" :key="child">
+              <a href="" v-if="!child.children"> {{child.name}} </a>
+              <a href="#" v-if="child.children"> {{child.name}} <span><i class="fas fa-caret-right"></i> </span> </a>
+              <ul v-if="child.children">
+                  <li v-for="child1 in child.children" :key="child1" style="margin-left: -28px;">
+                    <a href=""> {{child1.name}} </a>
+                  </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li class="right">
+          <a href="#" class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Ingresar </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <form class="px-4 py-3">
+            <div class="form-group">
+              <label for="exampleDropdownFormEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+            </div>
+            <div class="form-group">
+              <label for="exampleDropdownFormPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
+              <label class="form-check-label" for="dropdownCheck">
+                Remember me
+              </label>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </form>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">New around here? Sign up</a>
+          <a class="dropdown-item" href="#">Forgot password?</a>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-import { $ } from 'jquery';
 import UserService from "../services/user.service";
+import { FontAwesomeIcon } from '../plugins/font-awesome';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-    name: "Home",
-    data() {
-        return {
-            menuItems: [
+  name: "Home",
+  components: {
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      primaryMenu: [
+        {
+          name: "Inicio",
+        },
+        {
+          name: "Junta Directiva",
+          children: [
+            { name: "Junta Directiva" },
+            { name: "Órganos Electivos" },
+            {
+              name: "Comité de Damas",
+              children: [
+                { name: "Escuela de Sevillanas" },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Comité Juvenil",
+            },
+          ],
+        },
+        {
+          name: "Direcciones",
+          children: [
+            {
+              name: "Dirección de Admisión y Órden Social",
+              children: [
+                { name: "Planillas para descargar" },
+                { name: "Trámites" },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Dirección de Asistencia Social",
+              children: [
+                { name: "Centro de Día Santiago Apostol" },
+                { name: "Jornadas de Salud" },
+                { name: "Directorio Médico" },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Dirección de Cultura",
+              children: [
+                { name: "Planillas para descargar" },
+                { name: "Grupos Folklóricos" },
+                { name: "Teatro" },
+                { name: "Corales" },
+                { name: "Gaitas" },
+                { name: "Ballet" },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Dirección de Deportes",
+              children: [
+                { name: "Gimnasio" },
+                { name: "Actividades Deportivas" },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Dirección de Fiestas",
+              children: [
+                { name: "Día de Galicia" },
+                { name: "Noite de San Xoan" },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Obras y Mantenimiento",
+              children: [
+                { name: "Mantenimiento y Remodelaciones" },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Dirección de Relaciones Públicas",
+              children: [
+                { name: 'Boletín "Somos Hermandad"' },
+                { name: "Contacto" },
+              ],
+            },
+            {
+              name: "Oficina de Administración",
+            },
+          ],
+        },
+        {
+          name: "Estatutos y Reglamentos",
+          children: [
+            {
+              name: "Estatutos",
+            },
+            {
+              name: "Reglamento de la Cámara de Representantes",
+            },
+          ],
+        },
+        {
+          name: "Servicios",
+          children: [
+            {
+              name: "Servicios Médicos",
+            },
+            {
+              name: "Restaurantes y Cafeterías",
+            },
+            {
+              name: "Área Comercial",
+            },
+          ],
+        },
+        {
+          name: "OBAS",
+          children: [
+            {
+              name: "Planillas para Descargar",
+            },
+            {
+              name: "Programas",
+            },
+            {
+              name: "Contacto",
+            },
+            {
+              name: "Convocatorias",
+            },
+          ],
+        },
+        {
+          name: "Conócenos",
+          children: [
+            {
+              name: "Historia",
+            },
+            {
+              name: "Sedes",
+              children: [
                 {
-                    name: 'Item 1',
-                    children: [
-                            {name: 'Subitem 1'},
-                            {name: 'Subitem 2'},
-                            {name: 'Subitem 3'}
-                        ]
+                  name: "Maripérez",
                 },
                 {
-                    name: 'Item 2'
-                }
-            ],
-            selectedDropdown: 'None',
-            content: "",
-        };
-    },
-    mounted() {
-        UserService.getPublicContent().then(
-            (response) => {
-                this.content = response.data;
+                  name: "Valle Fresco",
+                },
+              ],
             },
-            (error) => {
-                this.content =
-                (error.response &&
-                    error.response.data &&
-                    error.response.data.message) ||
-                error.message ||
-                error.toString();
-            }
-        );
-    },
-    methods: {
-        currentDate() {
-            return new Date().toLocaleString();
+          ],
         },
-        setSelectedItem(item) {
-            this.selectedDropdown = item;
-        }
+        {
+          name: "Prensa Española",
+          children: [
+            {
+              name: "Magazine Español",
+            },
+            {
+              name: "La Voz de Galicia",
+            },
+            {
+              name: "La Región Internacional",
+            },
+            {
+              name: "Galicia en el Mundo",
+            },
+          ],
+        },
+      ],
+      selectedDropdown: "None",
+      content: "",
+    };
+  },
+  mounted() {
+    UserService.getPublicContent().then(
+      (response) => {
+        this.content = response.data;
+      },
+      (error) => {
+        this.content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  },
+  methods: {
+    currentDate() {
+      return new Date().toLocaleString();
     },
-    ready: function() {
-        $('.drop*down-submenu a.test').on("click", function(e){
-            $(this).next('ul').toggle();
-            e.stopPropagation();
-            e.preventDefault();
-        });    
-    }
+    setSelectedItem(item) {
+      console.log("item selected" + item);
+      this.selectedDropdown = item;
+    },
+  },
 };
 </script>
 
 <style>
-.dropdown-submenu {
-    position: relative;
+
+#menuDD {
+  margin-top: 10px;
 }
 
-.dropdown-submenu .dropdown-menu {
-    top: 0;
-    left: 100%;
-    margin-top: -1px;
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
+
+ul li {
+  display: block;
+  position: relative;
+  float: left;
+}
+
+li ul { display: none; }
+
+ul li a {
+  display: block;
+  text-decoration: none;
+  white-space: nowrap;
+  padding: 1em;
+  color: #000;
+}
+
+ul li a:hover { 
+  background: #da1919; 
+  color: yellow; 
+}
+
+li:hover > ul {
+  display: block;
+  position: absolute;
+}
+
+li:hover li { float: none; }
+
+li:hover a {     
+  background: rgb(255,255,255);
+  background: -moz-linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(0,158,224,1) 100%);
+  background: -webkit-linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(0,158,224,1) 100%);
+  background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(0,158,224,1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#ffffff",endColorstr="#009ee0",GradientType=1);
+}
+
+
+.main-navigation li ul li { border-top: 0; }
+
+ul ul ul {
+  left: 100%;
+  top: 0;
+}
+
+ul:before,
+ul:after {
+  content: " "; /* 1 */
+  display: table; /* 2 */
+}
+
+ul:after { clear: both; }
+
 </style>
